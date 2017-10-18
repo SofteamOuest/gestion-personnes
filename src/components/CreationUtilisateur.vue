dff9c7774e8bd7f313386f2913be529903d8cd70
Le Galludec Sebastien <sebastien.legalludec@softeam.fr>
2017-10-18 14:01:07 +0200

    Modification du formulaire de creation

--- src/components/CreationUtilisateur.vue
+++ src/components/CreationUtilisateur.vue
@@ -1,57 +1,71 @@
 <template>
-  <v-dialog v-model="dialog" persistent max-width="500px">
+  <v-dialog v-model="dialog" persistent max-width="800px">
     <v-btn icon class="menu-title" slot="activator">
       <v-icon>add</v-icon>
     </v-btn>
     <v-card>
       <v-card-title>
-        <div class="headline">User Profile</div>
+        <div class="headline">Ajouter une nouvelle personne</div>
       </v-card-title>
       <v-card-text>
         <v-container grid-list-md>
           <v-layout wrap>
-            <v-flex xs12 sm6 md4>
-              <v-text-field label="Legal first name" required></v-text-field>
+
+            <!-- Ligne Nom/Prenom-->
+            <v-flex xs12 sm6 md6>
+              <v-text-field label="Nom" v-model="personne.nom" required></v-text-field>
             </v-flex>
-            <v-flex xs12 sm6 md4>
-              <v-text-field label="Legal middle name" hint="example of helper text only on focus"></v-text-field>
+            <v-flex xs12 sm6 md6>
+              <v-text-field label="Prénom" v-model="personne.prenom" required></v-text-field>
             </v-flex>
-            <v-flex xs12 sm6 md4>
-              <v-text-field label="Legal last name" hint="example of persistent helper text"
-                            persistent-hint
-                            required
-              ></v-text-field>
+
+            <!-- Ligne Date de naissance / Poste occupé-->
+            <v-flex xs12 sm6 md6>
+              <v-text-field label="Date de Naissance" v-model="personne.dateNaissance" prepend-icon="date_range"
+                            required></v-text-field>
             </v-flex>
+            <v-flex xs12 sm6 md6>
+              <v-text-field label="Poste occupé" v-model="personne.poste" prepend-icon="event_seat"
+                            required></v-text-field>
+            </v-flex>
+
+            <!-- Ligne Email pro / Telephone pro -->
+            <v-flex xs12 sm6 md6>
+              <v-text-field label="Email pro" v-model="personne.emailPro" prepend-icon="mail" required></v-text-field>
+            </v-flex>
+            <v-flex xs12 sm6 md6>
+              <v-text-field label="Téléphone pro" v-model="personne.telephonePro" prepend-icon="phone"
+                            required></v-text-field>
+            </v-flex>
+
+            <!-- Ligne Email perso / Telephone perso -->
+            <v-flex xs12 sm6 md6>
+              <v-text-field label="Email perso" v-model="personne.emailPerso" prepend-icon="mail"
+                            required></v-text-field>
+            </v-flex>
+            <v-flex xs12 sm6 md6>
+              <v-text-field label="Téléphone perso" v-model="personne.telephonePerso" prepend-icon="phone"
+                            required></v-text-field>
+            </v-flex>
+
+            <!-- Ligne Photo avec miniature -->
             <v-flex xs12>
-              <v-text-field label="Email" required></v-text-field>
+              <v-icon>add_a_photo</v-icon>
             </v-flex>
+
+            <!-- Ligne Commentaire -->
             <v-flex xs12>
-              <v-text-field label="Password" type="password" required></v-text-field>
+              <v-text-field label="Commentaire" v-model="personne.commentaire" multi-line></v-text-field>
             </v-flex>
-            <v-flex xs12 sm6>
-              <v-select
-                label="Age"
-                required
-                :items="['0-17', '18-29', '30-54', '54+']"
-              ></v-select>
-            </v-flex>
-            <v-flex xs12 sm6>
-              <v-select
-                label="Interests"
-                multiple
-                autocomplete
-                chips
-                :items="['Skiing', 'Ice hockey', 'Soccer', 'Basketball', 'Hockey', 'Reading', 'Writing', 'Coding', 'Basejump']"
-              ></v-select>
-            </v-flex>
+
           </v-layout>
         </v-container>
         <small>* champs obligatoires</small>
       </v-card-text>
       <v-card-actions>
         <v-spacer></v-spacer>
-        <v-btn color="blue darken-1" flat @click.native="dialog = false">Close</v-btn>
-        <v-btn color="blue darken-1" flat @click.native="dialog = false">Save</v-btn>
+        <v-btn color="blue darken-1" flat @click.native="dialog = false">Retour</v-btn>
+        <v-btn color="blue darken-1" flat @click.native="ajouterNouvellePersonne();dialog = false">Ajouter</v-btn>
       </v-card-actions>
     </v-card>
   </v-dialog>
@@ -61,7 +75,14 @@
   export default {
     data() {
       return {
-        dialog: true
+        dialog: true,
+        image: '',
+        personne: {}
+      }
+    },
+    methods: {
+      ajouterNouvellePersonne() {
+        console.log('ajouterNouvellePersonne :: ' + this.personne);
       }
     }
   }
@@ -69,5 +90,9 @@
 
 <!-- Le scope CSS est isolé et uniquement disponible pour ce composant -->
 <style scoped>
+
+  #upload-file {
+    text-align: center;
+  }
 
 </style>
