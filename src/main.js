8216fa6d9e063bec6e7828d17e03274e037291d6
tgonce <thibault.gonce@softeam.fr>
2018-02-13 17:51:39 +0100

    import vuetify min.css

--- src/main.js
+++ src/main.js
@@ -7,7 +7,7 @@ import Vuetify from 'vuetify'
 import VueI18n from 'vue-i18n'
 import Vuex from 'vuex'
 import moment from 'moment'
-import '/node_modules/vuetify/dist/vuetify.min.css'
+import '../node_modules/vuetify/dist/vuetify.min.css'
 
 Vue.use(Vuetify)
 Vue.use(VueI18n)
