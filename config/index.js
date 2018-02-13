c7af8365cdfa03bdbbd605921c0be6e2b5628cc5
tgonce <thibault.gonce@softeam.fr>
2018-02-13 17:57:38 +0100

    index.js

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/gestion-personnes-vue/',
+    assetsPublicPath: 'gestion-personnes-vue/',
 
     /**
      * Source Maps
